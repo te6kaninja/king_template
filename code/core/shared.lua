70be2076e6da26b4ef7bbc56fd8a3908fb139a3c
Georgi Stoyanov <stoyanow2707@gmail.com>
2024-11-17 19:31:01 +0200

    feat(core): requireMod function

--- code/core/shared.lua
+++ code/core/shared.lua
@@ -3,18 +3,24 @@ local modules <const>, resource <const>, context <const> = {
     'misc'
 }, cache.resource, lib.context
 
-local globalVarName <const>, modsPath <const> = select(2, string.strsplit('_', resource)), ('@%s/code/modules'):format(resource)
+local globalVarName <const>, modulesPath <const> = select(2, string.strsplit('_', resource)), ('@%s/code/modules'):format(resource)
 _ENV[globalVarName] = {
-    bridge = lib.require(('%s/bridge/%s'):format(modsPath, context)),
+    bridge = lib.require(('%s/bridge/%s'):format(modulesPath, context)),
     config = lib.require(('%s/config/config'):format(resource)),
     -- Shared values
 }
 
 CreateThread(function()
-    local defaultPath <const> = ('%s/%s/%s'):format(modsPath, '%s', context)
     for i = 1, #modules do
-        _ENV[globalVarName][modules[i]] = {}
-        lib.require(defaultPath:format(modules[i]))
+        local moduleName <const> = modules[i]
+
+        _ENV[globalVarName][moduleName] = {
+            requireMod = function(mod)
+                return lib.require(('%s/%s/mods/%s'):format(modulesPath, moduleName, mod))
+            end
+        }
+
+        lib.require(('%s/%s/%s'):format(modulesPath, '%s', context):format(moduleName))
     end
 
     for name, func in pairs(_ENV[globalVarName]) do
