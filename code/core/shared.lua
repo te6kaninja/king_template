0bce82226ecf5ec3e01507ca5078e4b96316c9c1
Georgi Stoyanov <stoyanow2707@gmail.com>
2024-11-17 19:23:52 +0200

    feat(core): fetching the config and bridge and assigning them to the global variable

--- code/core/shared.lua
+++ code/core/shared.lua
@@ -1,15 +1,17 @@
-local modules <const>, resource <const> = {
+local modules <const>, resource <const>, context <const> = {
     'main',
     'misc'
-}, cache.resource
+}, cache.resource, lib.context
 
 local globalVarName <const>, modsPath <const> = select(2, string.strsplit('_', resource)), ('@%s/code/modules'):format(resource)
 _ENV[globalVarName] = {
+    bridge = lib.require(('%s/bridge/%s'):format(modsPath, context)),
+    config = lib.require(('%s/config/config'):format(resource)),
     -- Shared values
 }
 
 CreateThread(function()
-    local defaultPath <const> = ('%s/%s/%s'):format(modsPath, '%s', lib.context)
+    local defaultPath <const> = ('%s/%s/%s'):format(modsPath, '%s', context)
     for i = 1, #modules do
         _ENV[globalVarName][modules[i]] = {}
         lib.require(defaultPath:format(modules[i]))
