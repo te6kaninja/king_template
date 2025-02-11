bcf291a1101fbf2789634baadf084df2c43ae071
Georgi Stoyanov <stoyanow2707@gmail.com>
2025-02-11 20:06:43 +0200

    feat(server): added automatic debug convar setter for ox_libs print module

--- code/core/server.lua
+++ code/core/server.lua
@@ -1,1 +1,7 @@
-lib.print.info('core/server.lua loaded!')
+local config = require('data.config')
+
+AddEventHandler('onResourceStart', function(resource)
+    if resource == GetCurrentResourceName() then
+        SetConvarReplicated(('ox:printlevel:%s'):format(resource), config.debug and 'debug' or 'info')
+    end
+end)
