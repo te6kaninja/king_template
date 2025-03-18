422bbd145a15ce6ce79a0f9cf99515d01e1b6f13
Georgi Stoyanov <stoyanow2707@gmail.com>
2025-03-18 13:42:09 +0200

    chore(server): use ox_libs cached resource name for some micro optimization

--- code/core/server.lua
+++ code/core/server.lua
@@ -1,7 +1,7 @@
 local config = require('data.config')
 
 AddEventHandler('onResourceStart', function(resource)
-    if resource == GetCurrentResourceName() then
+    if resource == cache.resource then
         SetConvarReplicated(('ox:printlevel:%s'):format(resource), config.debug and 'debug' or 'info')
     end
 end)
