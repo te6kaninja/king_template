6c9f2b985aec5359ac44634c3182ddf55d29a926
Georgi Stoyanov <stoyanow2707@gmail.com>
2025-04-16 19:14:53 +0300

    feat(police): add initial police job module with departments and settings

---
+++ data/departments.lua
@@ -0,0 +1,22 @@
+return {
+    police = {
+        nameTemplate = 'Police Department %s',
+        departments = {
+            {
+                zoneData = {
+                    coords = vec3(462.312, -991.937, 35.122),
+                    radius = 50.0,
+                    debug = true
+                },
+
+                duty = {
+                    {
+                        coords = vec3(441.673, -982.071, 31.539),
+                        distance = 1.5,
+                        interaction = 'text' -- text, target or 3dtext
+                    }
+                }
+            }
+        }
+    }
+}
