3530e8014d4fc4ddbf65ebc0079adde884da9e2a
Georgi Stoyanov <stoyanow2707@gmail.com>
2025-04-24 09:06:49 +0300

    refactor(server): simplify logging level determination in resource start event

--- code/core/server.lua
+++ code/core/server.lua
@@ -2,8 +2,6 @@ local config = require('data.config')
 
 AddEventHandler('onResourceStart', function(rsc)
     if rsc == cache.resource then
-        if config.debug then
-            SetConvarReplicated(('ox:printlevel:%s'):format(rsc), 'debug')
-        end
+        SetConvarReplicated(('ox:printlevel:%s'):format(rsc), config.debug and 'debug' or 'info')
     end
 end)
